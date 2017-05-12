9ada932ada35e835e87c56de084cf21224c34e27
thomassc <thomsch98@gmail.com>
2017-05-12 21:06:48 +0200

    Initial

---
+++ 7-fpm-alpine/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -n "${XDEBUG_CONFIG}" ] ; then
+# XDEBUG_CONFIG="remote_host=172.17.0.1 remote_enable=1"
+
+    if [ -n "${PHP_IDE_CONFIG}" ] ; then
+    # PHP_IDE_CONFIG="serverName=some-server"
+        (echo "${PHP_IDE_CONFIG}" | grep -q serverName) || echo "Warning: PHP_IDE_CONFIG does NOT contain serverName=..."
+    endif
+
+    (echo "${XDEBUG_CONFIG}" | grep -q remote_host) && (echo "${XDEBUG_CONFIG}" | grep -q remote_enable) || echo "XDEBUG_CONFIG MUST contain remote_host=... and remote_enable=1"
+
+    echo "zend_extension=xdebug.so" > /usr/local/etc/php/conf.d/xdebug.ini
+    echo "PHP_IDE_CONFIG='${PHP_IDE_CONFIG}'. XDEBUG_CONFIG='${XDEBUG_CONFIG}'. xdebug ENABLED"
+else
+    echo "#zend_extension=xdebug.so" > /usr/local/etc/php/conf.d/xdebug.ini
+    echo "XDEBUG_CONFIG empty. xdebug DISABLED"
+fi
+
+echo "Now running '$@'" 
+
+exec "$@"
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # php7-composer-docker-image
+
+php7 installed on to of an Alpine base image.
+
+Additional modules (check with php -m for aomplete list)
+
+- xdebug 
+- mongodb 
+- intl
+- zip 
+- redis
+- apcu
+- pdo_mysql
+- sockets
+- opcache
+
+Special about this repo, if anything, is the entrypoint.sh file.
+If the environment variables XDEBUG_CONFIG is set, the xdebug extension is enabled at container startup 
+and the environment variable controls where [xdebug sends its data](https://xdebug.org/docs/remote).
+
+If PHPStorm is being used, or any other IDE that adheres to the [settings of PHP_IDE_CONFIG](https://www.jetbrains.com/help/phpstorm/2017.1/zero-configuration-debugging.html).
+
+This helps to use the identical container at development (remote debugging) and production time (no performace impact because of xdebug)
